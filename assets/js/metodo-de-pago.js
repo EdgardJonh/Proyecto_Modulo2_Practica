7c38a6eafb19e0f66a327b8d311370d6b98bd934
Edgard Carrion <edgard.jonthcs@gmail.com>
2024-07-28 22:31:10 -0400

    Se realizan cambios en el formulario metodo de pago

--- assets/js/metodo-de-pago.js
+++ assets/js/metodo-de-pago.js
@@ -1,37 +1,90 @@
-const credi = document.getElementById('credito').value
-const debi = document.getElementById('debito').value
-const tranf = document.getElementById('tranfer').value
+const credi = document.getElementById("credito");
+const debi = document.getElementById("debito");
+const tranf = document.getElementById("tranfer");
 
-const padreForm = document.getElementById('padreForm')
+const padreForm = document.getElementById("padreForm");
 
-document.addEventListener('click',function(e){
-    console.log(e.target.value)
-    if (e.target.value == 'credito') {
-        padreForm.innerHTML=`     <form>
+document.addEventListener("click", function (e) {
+  const formu = `   <form id="formulario-usuario"> 
                             <div class="form-group">
                                 <label for="nombre">Nombre del titular:</label>
-                                <input type="text" class="form-control" id="nombre" required>
+                                <input type="text" class="form-control" id="nombre">
                             </div>
                             <div class="form-group">
                                 <label for="numero_tarjeta">Número de tarjeta:</label>
-                                <input type="number" class="form-control" id="numero_tarjeta" required>
+                                <input type="number" class="form-control" id="numero_tarjeta" >
                             </div>
                             <div class="form-group">
                                 <label for="fecha_vencimiento">Fecha de vencimiento:</label>
-                                <input type="month" class="form-control" id="fecha_vencimiento" required>
+                                <input type="month" class="form-control" id="fecha_vencimiento" >
                             </div>
                             <div class="form-group">
                                 <label for="codigo_seguridad">Código de seguridad:</label>
-                                <input type="number" class="form-control" id="codigo_seguridad" required>
+                                <input type="number" class="form-control" id="codigo_seguridad">
                             </div>
+                            <div id="mensaje-confirmacion"></div>
                             <button type="submit" class="btn btn-primary mt-2">Realizar pago</button>
                         </form>`
+  if (e.target.id == "credito") {
+    padreForm.innerHTML = `Seccion Credito ${formu}`;
+  }
+
+  const formulario = document.getElementById("formulario-usuario");
+  formulario.addEventListener("submit", function (event) {
+    event.preventDefault();
+    const mensajeConfirmacion = document.getElementById("mensaje-confirmacion");
+    const nombre = document.getElementById("nombre").value;
+    const numero_tarjeta = document.getElementById("numero_tarjeta").value;
+    const fecha_vencimiento =
+      document.getElementById("fecha_vencimiento").value;
+    const codigo_seguridad = document.getElementById("codigo_seguridad").value;
+    if (
+      nombre !== "" &&
+      numero_tarjeta !== "" &&
+      fecha_vencimiento !== "" &&
+      codigo_seguridad !== ""
+    ) {
+      mensajeConfirmacion.innerHTML = '<p class="text-success">Datos enviados</p>';
+    } else {
+      mensajeConfirmacion.innerHTML = '<p class="text-danger">Debes rellenar todos los campos.</p>';
     }
-    if(e.target.value == 'debito'){
-        padreForm.innerHTML= `seccion debito`
-    }
-    if(e.target.value == 'tranfer'){
-        padreForm.innerHTML= `seccion tranferencia`
-    }
-})
-console.log(credi)
+  });
+  if (e.target.id == "debito") {
+    padreForm.innerHTML = `Seccion Debito ${formu}`;
+  }
+  if (e.target.id == "tranfer") {
+    padreForm.innerHTML = `<form>
+		<div class="form-group row">
+		  <label for="staticEmail" class="col-sm-2 col-form-label">Nombre</label>
+		  <div class="col-sm-10">
+			<input type="text" readonly class="form-control-plaintext" id="staticEmail" value="Colegio E-BootCamp">
+		  </div>
+		</div>
+    <div class="form-group row">
+		  <label for="staticEmail" class="col-sm-2 col-form-label">RUT</label>
+		  <div class="col-sm-10">
+			<input type="text" readonly class="form-control-plaintext" id="staticEmail" value="45.343.343-3">
+		  </div>
+		</div>
+    <div class="form-group row">
+		  <label for="staticEmail" class="col-sm-2 col-form-label">Cuenta</label>
+		  <div class="col-sm-10">
+			<input type="text" readonly class="form-control-plaintext" id="staticEmail" value="12434353">
+		  </div>
+		</div>
+    <div class="form-group row">
+		  <label for="staticEmail" class="col-sm-2 col-form-label">Banco</label>
+		  <div class="col-sm-10">
+			<input type="text" readonly class="form-control-plaintext" id="staticEmail" value="Banco Estado">
+		  </div>
+		</div>
+     <div class="form-group row">
+		  <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+		  <div class="col-sm-10">
+			<input type="text" readonly class="form-control-plaintext" id="staticEmail" value="E-bootCamp@e-camp.cl">
+		  </div>
+		</div>
+		
+	  </form>`;
+  }
+});
